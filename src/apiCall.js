f9e6d6d6eb4fd71b59ccf12f149660f0ae1329e1
jpzingoni <juanzingoni@gmail.com>
2023-08-12 23:15:45 -0300

    Se agregan modificaciones a los archivos con packages.json que agregaron. Se moodificaron estilos y se agrega un buscador de pokemons. Funcionabilidad limitada, hay errores a corregir

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,119 +1,125 @@
+/* eslint-disable func-names */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-return-assign */
+/* eslint-disable no-undef */
 function obtenerNumeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let numeroAleatorio = obtenerNumeroAleatorio(1, 150); // Número entre 1 y 10
-let numeroActual = numeroAleatorio
-let URLPOKEMONES = `https://pokeapi.co/api/v2/pokemon/${numeroAleatorio}`
-console.log(numeroAleatorio);
+const numeroAleatorio = obtenerNumeroAleatorio(1, 150); // Número entre 1 y 10
+let numeroActual = numeroAleatorio;
+let URLPOKEMONES = `https://pokeapi.co/api/v2/pokemon/${numeroAleatorio}`;
 
-const URLLISTAPOKEMONS = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=151`
+const URLLISTAPOKEMONS = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=151';
 
 let LISTAPOKEMONS = fetch(URLLISTAPOKEMONS)
-    .then(response =>response.json())
-    .then(data => LISTAPOKEMONS = data.results)
+  .then((response) => response.json())
+  .then((data) => (LISTAPOKEMONS = data.results));
 
 let POKEMONES = fetch(URLPOKEMONES)
-    .then(response => response.json())
-    .then(data => POKEMONES = data)
-    .catch(error => "hubo un problema de error: " + error)
+  .then((response) => response.json())
+  .then((data) => (POKEMONES = data))
+  .catch((error) => `hubo un problema de error: ${error}`);
 
-let nombresPokemons = (nombre) =>{
-    const $nombrePokemon = document.getElementById("nombre-pokemon")
-    return $nombrePokemon.innerText = `NOMBRE: ${nombre}`
-}
+const nombresPokemons = (nombre) => {
+  const $nombrePokemon = document.getElementById('nombre-pokemon');
+  $nombrePokemon.innerText = `NOMBRE: ${nombre}`;
+};
 
-let numeroPokemons = (numero) =>{
-    const $numeroPokemon = document.getElementById("numero-pokemon")
-    return $numeroPokemon.innerText = `ID: ${numero}`
-}
+const numeroPokemons = (numero) => {
+  const $numeroPokemon = document.getElementById('numero-pokemon');
+  $numeroPokemon.innerText = `ID: ${numero}`;
+};
 
-let imagenPokemon = (imagen) =>{
-    const $imagenPokemon = document.getElementById("imagen-pokemon");
-    return $imagenPokemon.src = imagen
-}
+const imagenPokemon = (imagen) => {
+  const $imagenPokemon = document.getElementById('imagen-pokemon');
+  $imagenPokemon.src = imagen;
+};
 
-let alturaPokemon = (altura) =>{
-    const $alturaPokemon = document.getElementById("altura-pokemon");
-    let alturaEnMetros = altura / 10
-    return $alturaPokemon.innerText = `Altura: ${alturaEnMetros} M`
-}
+const alturaPokemon = (altura) => {
+  const $alturaPokemon = document.getElementById('altura-pokemon');
+  const alturaEnMetros = altura / 10;
+  $alturaPokemon.innerText = `Altura: ${alturaEnMetros} M`;
+};
 
-let pesoPokemon = (peso) =>{
-    const $pesoPokemon = document.getElementById("peso-pokemon");
-    let pesoEnKilos = peso / 10
-    return $pesoPokemon.innerText = `Peso: ${pesoEnKilos} KG`
-}
+const pesoPokemon = (peso) => {
+  const $pesoPokemon = document.getElementById('peso-pokemon');
+  const pesoEnKilos = peso / 10;
+  $pesoPokemon.innerText = `Peso: ${pesoEnKilos} KG`;
+};
 
-let tipoPokemon = (tipo) =>{
-    const $tipoPokemon = document.getElementById("tipo-pokemon");
-    let $capturarTipo =[]
-    tipo.forEach(e => {
-        $capturarTipo.push(e.type.name)
-    })
-    if($capturarTipo.length === 1){
-        let $textoTipo = `Tipo: ${$capturarTipo.join("")}`
-        return $tipoPokemon.innerText = $textoTipo
-    }else{
-        let $textoTipo = `Tipos: ${$capturarTipo.join("/")}`
-        return $tipoPokemon.innerText = $textoTipo
-    }
-}
+const tipoPokemon = (tipo) => {
+  const $tipoPokemon = document.getElementById('tipo-pokemon');
+  const $capturarTipo = [];
+  tipo.forEach((e) => {
+    $capturarTipo.push(e.type.name);
+  });
+  if ($capturarTipo.length === 1) {
+    const $textoTipo = `Tipo: ${$capturarTipo.join('')}`;
+    $tipoPokemon.innerText = $textoTipo;
+  } else {
+    const $textoTipo = `Tipos: ${$capturarTipo.join('/')}`;
+    ($tipoPokemon.innerText = $textoTipo);
+  }
+};
 
-let listaPokemones = (LISTAPOKEMONS) =>{
-    const $listaDesplegable = document.getElementById("pokemon-list")
-    const $pokemonSelecionado = document.getElementById("pokemon-seleccionado")
-    $pokemonSelecionado.innerText = POKEMONES.name
-    LISTAPOKEMONS.forEach(pokemon =>{
-        const $option = document.createElement("option");
-        $option.value = pokemon.name
-        $option.textContent = pokemon.name
-        $listaDesplegable.appendChild($option)
-    })
+const listaPokemones = (LISTAPOKEMONS) => {
+  const $listaDesplegable = document.getElementById('pokemon-list');
+  const $pokemonSelecionado = document.getElementById('pokemon-seleccionado');
+  $pokemonSelecionado.innerText = POKEMONES.name;
+  LISTAPOKEMONS.forEach((pokemon) => {
+    const $option = document.createElement('option');
+    $option.value = pokemon.name;
+    $option.textContent = pokemon.name;
+    $listaDesplegable.appendChild($option);
+  });
+};
 
-}
+const cambiarPokemonAnterior = () => {
+  numeroActual -= 1;
+  URLPOKEMONES = `https://pokeapi.co/api/v2/pokemon/${numeroActual}`;
+  POKEMONES = fetch(URLPOKEMONES)
+    .then((response) => response.json())
+    .then((data) => (POKEMONES = data))
+    .catch((error) => `hubo un problema de error: ${error}`);
+  cargarPokemon();
+};
 
-let cambiarPokemonAnterior = () =>{
-    numeroActual = numeroActual - 1
-    URLPOKEMONES = `https://pokeapi.co/api/v2/pokemon/${numeroActual}`
-    POKEMONES = fetch(URLPOKEMONES)
-    .then(response => response.json())
-    .then(data => POKEMONES = data)
-    .catch(error => "hubo un problema de error: " + error)
-    cargarPokemon()
-}
+const cambiarPokemonSiguiente = () => {
+  numeroActual += 1;
+  URLPOKEMONES = `https://pokeapi.co/api/v2/pokemon/${numeroActual}`;
+  POKEMONES = fetch(URLPOKEMONES)
+    .then((response) => response.json())
+    .then((data) => (POKEMONES = data))
+    .catch((error) => `hubo un problema de error: ${error}`);
+  cargarPokemon();
+};
 
-let cambiarPokemonSiguiente = () =>{
-    numeroActual = numeroActual + 1
-    URLPOKEMONES = `https://pokeapi.co/api/v2/pokemon/${numeroActual}`
-    POKEMONES = fetch(URLPOKEMONES)
-    .then(response => response.json())
-    .then(data => POKEMONES = data)
-    .catch(error => "hubo un problema de error: " + error)
-    cargarPokemon()
-}
+const cambiarOpacidad = () => {
+  const content = document.querySelector('.content');
+  content.style.opacity = 0;
+  setTimeout(() => {
+    // Restaurar la opacidad después de 2 segundos
+    content.style.opacity = 1;
+  }, 1600);
+};
 
-let cambiarOpacidad = ()=>{
-    const content = document.querySelector(".content");
-    content.style.opacity = 0;
-    setTimeout(function () {
-        // Restaurar la opacidad después de 2 segundos
-        content.style.opacity = 1;
-    }, 1600);
-}
-
-let buscarPokemons = () =>{
-    const $buscarPokemon = document.getElementById("buscar-pokemon")
-    let $pokemonSeleccionado = document.getElementById("pokemon-list")
-    $buscarPokemon.onclick = function(){
-        const pokemonABuscar = LISTAPOKEMONS.find(pokemon => pokemon.name === $pokemonSeleccionado.value)
-        POKEMONES = fetch(pokemonABuscar.url)
-        .then(response => response.json())
-        .then(data => POKEMONES = data)
-        .catch(error => "hubo un problema de error: " + error)
-        cargarPokemon()
-        cambiarOpacidad()
-        setTimeout(function(){
-            numeroActual = POKEMONES.id},1000)
-    }
-}
+const buscarPokemons = () => {
+  const $buscarPokemon = document.getElementById('boton-buscar-pokemon');
+  const $pokemonSeleccionado = document.getElementById('pokemon-list');
+  $buscarPokemon.onclick = function () {
+    const pokemonABuscar = LISTAPOKEMONS.find(
+      (pokemon) => pokemon.name === $pokemonSeleccionado.value,
+    );
+    POKEMONES = fetch(pokemonABuscar.url)
+      .then((response) => response.json())
+      .then((data) => (POKEMONES = data))
+      .catch((error) => `hubo un problema de error: ${error}`);
+    cargarPokemon();
+    cambiarOpacidad();
+    setTimeout(() => {
+      numeroActual = POKEMONES.id;
+    }, 1000);
+  };
+};
--- src/script.js
+++ src/script.js
@@ -1,38 +1,42 @@
-const $FLECHAIZQUIERDA = document.getElementById("flecha-izquierda")
-const $FLECHADERECHA = document.getElementById("flecha-derecha")
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+const $FLECHAIZQUIERDA = document.getElementById('flecha-izquierda');
+const $FLECHADERECHA = document.getElementById('flecha-derecha');
 
-setTimeout(function () {
-    // Oculta la imagen de carga
-    const loadingImage = document.getElementById("loading-image");
-    loadingImage.style.display = "none";
+setTimeout(() => {
+  // Oculta la imagen de carga
+  const loadingImage = document.getElementById('loading-image');
+  loadingImage.style.display = 'none';
 
-    // Muestra el contenido con una transición suave
-    const content = document.querySelector(".content");
-    const border = document.getElementById("borde-contenedor")
-    content.classList.remove("hidden");
-    border.classList.remove("hidden");
-    content.style.opacity = 1;
-  }, 2000); // 2000 ms = 2 segundos
+  // Muestra el contenido con una transición suave
+  const content = document.querySelector('.content');
+  const border = document.getElementById('borde-contenedor');
+  content.classList.remove('hidden');
+  border.classList.remove('hidden');
+  content.style.opacity = 1;
+}, 2000); // 2000 ms = 2 segundos
 
-let cargarPokemon = ()=> {setTimeout(function(){
-        nombresPokemons(POKEMONES.name)
-        numeroPokemons(POKEMONES.id)
-        imagenPokemon(POKEMONES.sprites.front_default)
-        alturaPokemon(POKEMONES.height)
-        pesoPokemon(POKEMONES.weight)
-        tipoPokemon(POKEMONES.types)
-    },1300)}
+const cargarPokemon = () => {
+  setTimeout(() => {
+    nombresPokemons(POKEMONES.name);
+    numeroPokemons(POKEMONES.id);
+    imagenPokemon(POKEMONES.sprites.front_default);
+    alturaPokemon(POKEMONES.height);
+    pesoPokemon(POKEMONES.weight);
+    tipoPokemon(POKEMONES.types);
+  }, 1300);
+};
 
-$FLECHAIZQUIERDA.onclick = function(){
-    cambiarPokemonAnterior()
-    cambiarOpacidad()
-}
+$FLECHAIZQUIERDA.onclick = function () {
+  cambiarPokemonAnterior();
+  cambiarOpacidad();
+};
 
-$FLECHADERECHA.onclick = function(){
-    cambiarPokemonSiguiente()
-    cambiarOpacidad()
-}
+$FLECHADERECHA.onclick = function () {
+  cambiarPokemonSiguiente();
+  cambiarOpacidad();
+};
 
-cargarPokemon()
-setTimeout(() => listaPokemones(LISTAPOKEMONS),1300)
-setTimeout(() => buscarPokemons(), 1300)
+cargarPokemon();
+setTimeout(() => listaPokemones(LISTAPOKEMONS), 1300);
+setTimeout(() => buscarPokemons(), 1300);
